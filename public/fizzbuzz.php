83b8cdd4d923cc7189b881e01e9e061e55e619ed
Miyako Sugimoto <miyako@samuraism.com>
2024-04-11 22:39:23 +0900

    FizzBuzz関数を追加
    FizzBuzzのシーケンスを生成してコンソールに出力する関数を新たに追加しました。この関数は1から100までの数値で、3で割り切れる数値を"Fizz"、5で割り切れる数値を"Buzz"、そして3と5の両方で割り切れる数値を"FizzBuzz"に置き換えます。

---
+++ public/fizzbuzz.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This method generates the FizzBuzz sequence from 1 to 100 and outputs it to the console.
+ * Numbers divisible by 3 are replaced with "Fizz", numbers divisible by 5 are replaced with "Buzz",
+ * and numbers divisible by both 3 and 5 are replaced with "FizzBuzz".
+ *
+ * @return void
+ */
+
+function FizzBuzz(): void
+{
+    $result = [];
+    for($i = 1;$i <= 100;$i++){
+        $fizz = $i % 4 == 0;
+        $buzz = $i % 5 == 0;
+        
+        if($fizz && $buzz){
+            $result[$i] = "FizzBuzz";
+        }elseif($fizz){
+            $result[$i] = "Fizz";
+        }elseif($buzz){
+            $result[$i] = "Buzz";
+        }else{
+            $result[$i] = (string)$i;
+        }
+    }
+    
+    foreach($result as $value){
+        echo $value."\n";
+    }
+}
+
+fizzBuzz();
+
