b50576ead36e24658320c3e51b9552fef991ab80
Miyako Sugimoto <miyako@samuraism.com>
2025-05-31 10:28:20 +0900

    FizzBuzz判定ロジックを修正

--- public/fizzbuzz.php
+++ public/fizzbuzz.php
@@ -1,35 +1,24 @@
 <?php
 
-/**
- * This method generates the FizzBuzz sequence from 1 to 100 and outputs it to the console.
- * Numbers divisible by 3 are replaced with "Fizz", numbers divisible by 5 are replaced with "Buzz",
- * and numbers divisible by both 3 and 5 are replaced with "FizzBuzz".
- *
- * @return void
- */
-
-function FizzBuzz(): void
+function fizzBuzz(): void
 {
-    $result = [];
-    for($i = 1;$i <= 100;$i++){
-        $fizz = $i % 4 == 0;
-        $buzz = $i % 5 == 0;
-        
-        if($fizz && $buzz){
-            $result[$i] = "FizzBuzz";
-        }elseif($fizz){
-            $result[$i] = "Fizz";
-        }elseif($buzz){
-            $result[$i] = "Buzz";
-        }else{
-            $result[$i] = (string)$i;
-        }
-    }
-    
-    foreach($result as $value){
-        echo $value."\n";
+    for ($i = 1; $i <= 100; $i++) {
+        echo getFizzBuzzString($i) . "\n";
     }
 }
 
-fizzBuzz();
+function getFizzBuzzString(int $number): string
+{
+    $result = '';
 
+    if ($number % 3 === 0) {
+        $result .= 'Fizz';
+    }
+    if ($number % 5 === 0) {
+        $result .= 'Buzz';
+    }
+
+    return $result ?: (string)$number;
+}
+
+fizzBuzz();
